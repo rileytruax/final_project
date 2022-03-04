2869bafce6399fd6e8dd52efd850b56b3713480c
Riley <rmtruax15@gmail.com>
2022-03-04 14:54:42 -0600

    first commit

--- homepage.php
+++ homepage.php
@@ -9,7 +9,9 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Homepage</title>
-    
+    <style>
+        
+    </style>
 </head>
 <body>
     <header>
