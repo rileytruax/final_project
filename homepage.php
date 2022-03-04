4393bb668944f1ec157ad348eb0fa5f1f27b7f0d
Riley <rmtruax15@gmail.com>
2022-03-04 14:52:07 -0600

    first commit starting login page

---
+++ homepage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Movie Review Website">
+    <meta name="keywords" content="movies, review">
+    <meta name="Author" content="Riley Truax">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Homepage</title>
+    
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">List</a>
+            <a href="">Reviews</a>
+            <a href="">Logout</a>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ loginpage.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Movie Review Website">
+    <meta name="keywords" content="movies, review">
+    <meta name="Author" content="Riley Truax">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: beige;
+        }
+        .header{
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+        .child {
+            text-align: center;
+        }
+        .form {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 750px;
+        }
+        .input {
+            width: 250px;
+            margin-top: 5px;
+        }
+        .loginButton{
+            background-color: #71c77c;
+            border: none;
+            border-radius: 3px;
+            margin-top: 10px;
+            width: 250px;
+            font-weight: lighter;
+        }
+        .registerButton{
+            background-color: #F87357;
+            border: none;
+            border-radius: 3px;
+            width: 250px;
+            font-weight: lighter;
+        }
+        .register{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <header class="header">
+        <h2>Welcome To The Movie Reviewer!</h2>
+        <br>
+        <nav>
+            <a href="homepage.php">Homepage</a>
+        </nav>
+    </header>
+    <br>
+    <main>
+        <div class="child">
+            <p>Thank you for visiting our site. We are currently working on the content for this page, so please check back another time.</p>
+            <p>Thank you and have a great day!!</p>
+        </div>
+        <div class="form">
+            <form>
+                <input class="input" type="username" name="username" placeholder="Username" required>
+                <br>
+                <input class="input" type="password" name="passsword" placeholder="Password" required>
+                <br>
+                <button class="loginButton" type="submit" name="login">LOGIN</button>
+            </form>
+        </div>
+        <div class="register">
+            <button class="registerButton" type="submit" name="register">REGISTER</button>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
