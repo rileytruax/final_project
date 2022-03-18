34d02ff20dc4d0447bcb22c0cdb2e498d49ee86f
Riley <rmtruax15@gmail.com>
2022-03-18 14:57:15 -0500

    added titles, continued working on layout, creating database

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # final_project
 This project will be a movie review website.
 Users will be allowed to look thorugh reviews, wirte their own reviews, reply to other users or even add movies to their list.
+3/18 - continuing to work on pages, working on database and researching on hashing passwords and figuring out which method to use.
--- loginpage.php
+++ loginpage.php
@@ -10,9 +10,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300&display=swap" rel="stylesheet">
     <style>
-        body {
-            
-        }
         h2{
             font-family: 'Fredoka', sans-serif;
             font-weight: 700;
@@ -80,9 +77,8 @@
         </div>
         <div class="form">
             <form>
-                <input class="input" type="username" name="username" placeholder="Username" required>
-                <br>
-                <input class="input" type="password" name="passsword" placeholder="Password" required>
+                <input class="form-control" type="username" name="username" placeholder="Username" required>
+                <input class="form-control" type="password" name="passsword" placeholder="Password" required>
                 <br>
                 <button class="loginButton" type="submit" name="login">LOGIN</button>
             </form>
